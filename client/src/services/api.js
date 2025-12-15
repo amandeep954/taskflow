a7ef9bed95f08cc8573d2933f385b408afd0f2e8
Aman Deep <ad707863@gmail.com>
2025-12-15 10:30:44 +0530

    Fixed api export default issue

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-
-  baseURL: 'https://taskflow-api-uh3i.onrender.com/api', 
+  baseURL: "https://taskflow-api-uh3i.onrender.com/api",
 });
 
-
-
+export default api;
